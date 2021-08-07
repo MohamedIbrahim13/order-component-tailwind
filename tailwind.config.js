64cde96b6b219e858323ff114738b78436b74387
mohamed <mohamed.mohamed.yossef@gmail.com>
2021-08-07 20:52:50 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./*.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Red Hat Display"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
